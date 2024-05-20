11b2ff8ee876516e5fdbc232014d1ed173628364
Ivan <jarvis.zoorgut@gmail.com>
2024-05-20 22:16:39 +0500

    2024.05.13 - E4.4 done

--- E4/js/zadanie_04.js
+++ E4/js/zadanie_04.js
@@ -1,16 +1,28 @@
-function Consumer(state, current, voltage) {
-    this.plugIn = function (state) {
-        console.log(`The cunsumer ${this} is ${state} `)
-    }
-    this.getPower = function () {
-        console.log(`Power of ${this} is ${current * voltage}`)
-    }
+// Реализовать следующее консольное приложение подобно примеру, который разбирался в видео. Реализуйте его на прототипах.
+// Определить иерархию электроприборов. Включить некоторые в розетку. Посчитать потребляемую мощность.
+// Таких приборов должно быть, как минимум, два (например, настольная лампа и компьютер). Выбрав прибор, подумайте, какими свойствами он обладает.
+
+
+
+function Consumer(name, current, voltage) {
+    this.name = name;
+    this.current = current;
+    this.voltage = voltage;
+
+    this.switchOn = function () {
+        console.log(`${this.name} включен, потребляемая мощность ${this.current * this.voltage} Ватт`);
+    };
+
+    this.switchOff = function () {
+        console.log(`${this.name} выключен, потребляемая мощность 0 Ватт`);
+    };
 }
 
-const computer = new Consumer(3, 220);
-const mixer = new Consumer(1.5, 220);
+const computer = new Consumer('Компьютер', 4, 220);
+const mixer = new Consumer('Миксер', 1.5, 220);
 
-computer.getPower()
-mixer.getPower()
+computer.switchOn();
+mixer.switchOn();
 
-computer.plugIn(yes)
+computer.switchOff();
+mixer.switchOff();
