440535946f634d592b53778978f15769e73218bf
Ivan <jarvis.zoorgut@gmail.com>
2024-05-20 22:38:36 +0500

    2024.05.20 - E4.4 done

--- E4/js/zadanie_04.js
+++ E4/js/zadanie_04.js
@@ -1,28 +1,63 @@
-// Реализовать следующее консольное приложение подобно примеру, который разбирался в видео. Реализуйте его на прототипах.
-// Определить иерархию электроприборов. Включить некоторые в розетку. Посчитать потребляемую мощность.
-// Таких приборов должно быть, как минимум, два (например, настольная лампа и компьютер). Выбрав прибор, подумайте, какими свойствами он обладает.
-
-
-
-function Consumer(name, current, voltage) {
+function Appliance(name, power) {
     this.name = name;
-    this.current = current;
-    this.voltage = voltage;
-
-    this.switchOn = function () {
-        console.log(`${this.name} включен, потребляемая мощность ${this.current * this.voltage} Ватт`);
-    };
-
-    this.switchOff = function () {
-        console.log(`${this.name} выключен, потребляемая мощность 0 Ватт`);
-    };
+    this.power = power;
+    this.isOn = false;
 }
 
-const computer = new Consumer('Компьютер', 4, 220);
-const mixer = new Consumer('Миксер', 1.5, 220);
+Appliance.prototype.switchOn = function() {
+    this.isOn = true;
+    console.log(`${this.name} включен, потребляемая мощность ${this.power} Ватт`);
+};
 
+Appliance.prototype.switchOff = function() {
+    this.isOn = false;
+    console.log(`${this.name} выключен`);
+};
+
+function Lamp(name, power, brightness) {
+    Appliance.call(this, name, power);
+    this.brightness = brightness;
+}
+
+Lamp.prototype = Object.create(Appliance.prototype);
+Lamp.prototype.constructor = Lamp;
+
+Lamp.prototype.adjustBrightness = function(level) {
+    this.brightness = level;
+    console.log(`${this.name} яркость установлена на уровень ${this.brightness}`);
+};
+
+function Computer(name, power, ram) {
+    Appliance.call(this, name, power);
+    this.ram = ram;
+}
+
+Computer.prototype = Object.create(Appliance.prototype);
+Computer.prototype.constructor = Computer;
+
+Computer.prototype.upgradeRam = function(newRam) {
+    this.ram = newRam;
+    console.log(`${this.name} RAM увеличен до ${this.ram} GB`);
+};
+
+const lamp = new Lamp('Настольная лампа', 60, 100);
+const computer = new Computer('Компьютер', 300, 8);
+
+lamp.switchOn();
 computer.switchOn();
-mixer.switchOn();
 
-computer.switchOff();
-mixer.switchOff();
+lamp.adjustBrightness(75);
+computer.upgradeRam(16);
+
+lamp.switchOff();
+// computer.switchOff();
+
+function calculateTotalPower(appliances) {
+    return appliances.reduce((total, appliance) => {
+        return total + (appliance.isOn ? appliance.power : 0);
+    }, 0);
+}
+
+const appliances = [lamp, computer];
+const totalPower = calculateTotalPower(appliances);
+console.log(`Общая потребляемая мощность: ${totalPower} Ватт`);
